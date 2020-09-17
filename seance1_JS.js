dcede5064ad3078f5811583e08b78807179526d9
vdcmathieu <vandemathieu@gmail.com>
2020-09-17 16:22:34 +0200

    Correction: - when moving the cell on the right, left, up & down, the cell was only moving one cell at a time. The bug was corrected Implementation : - Fusion left, right, up & down

--- seance1_JS.js
+++ seance1_JS.js
@@ -7,16 +7,24 @@ document.addEventListener('keydown', function(event) {
     if(event.keyCode === 38) {
         console.log('haut');
         moveUp(0);
+        fusionUp(0);
         moveUp(1);
+        fusionUp(1);
         moveUp(2);
+        fusionUp(2);
         moveUp(3);
+        fusionUp(3);
     }
     else if(event.keyCode === 40) {
         console.log('bas');
         moveDown(0);
+        fusionDown(0);
         moveDown(1);
+        fusionDown(1);
         moveDown(2);
+        fusionDown(2);
         moveDown(3);
+        fusionDown(3);
     }
     else if(event.keyCode === 37) {
         console.log('gauche');
@@ -32,13 +40,13 @@ document.addEventListener('keydown', function(event) {
     else if(event.keyCode === 39) {
         console.log('droit');
         moveRight(0);
-        //fusionRight(0);
+        fusionRight(0);
         moveRight(1);
-        //fusionRight(1);
+        fusionRight(1);
         moveRight(2);
-        //fusionRight(2);
+        fusionRight(2);
         moveRight(3);
-        //fusionRight(3);
+        fusionRight(3);
     }
     else if (event.keyCode === 84){
         init();
@@ -132,79 +140,55 @@ function isEmpty(i,j) {
 }
 
 function moveRight(i) {
-    let newRow = ['','','',''];
-    let row = showRow(i);
-    let checker = false;
-    for (let j of row) {
-        if (j !== '') {checker = true}
-    }
-    while (checker === true) {
-        console.log('Row number'+i+': '+row)
-        for (let colm = 3;colm >= 0;colm--) {
-            if (row[colm+1] === '' && row[colm] !== '' && colm+1 < row.length) {
-                newRow[colm+1] = row[colm];
-                row[colm+1] = row[colm];
-                row[colm] = '';
-            }
-            else {newRow[colm] = row[colm]}
-        }
-        checker = false
-        for (let colm = 0;colm <= 3;colm++) {
-            if (newRow[colm] !== '' && newRow[colm+1] === '') {
-                checker = true
+    var row = showRow(i);
+    for (var k = 0; k <= 3; k++){
+        for (var j = 0; j <= 3; j++){
+            if ((row[j+1] === '') && (row[j] !== '')){
+                row[j+1] = row[j];
+                row[j] = '';
             }
         }
-        console.log('New row number'+i+': '+newRow)
     }
-    setRow(i,newRow[0],newRow[1],newRow[2],newRow[3])
+    setRow(i, row[0], row[1], row[2], row[3]);
 }
 
 function moveLeft(i) {
-    let newRow = ['','','',''];
-    let row = showRow(i);
-    console.log('Row number'+i+': '+row)
-    for (let colm = 0;colm <= 3;colm++) {
-        if (row[colm-1] === '' && row[colm] !== '' && colm-1 < row.length) {
-            newRow[colm-1] = row[colm];
-            row[colm-1] = row[colm];
-            row[colm] = '';
+    var row = showRow(i);
+    for (var k = 0; k <= 3; k++){
+        for (var j = 0; j <= 3; j++){
+            if ((row[j-1] === '') && (row[j] !== '')){
+                row[j-1] = row[j];
+                row[j] = '';
+            }
         }
-        else {newRow[colm] = row[colm]}
     }
-    console.log('New row number'+i+': '+newRow)
-    setRow(i,newRow[0],newRow[1],newRow[2],newRow[3])
+    setRow(i, row[0], row[1], row[2], row[3]);
 }
 
 function moveUp(j) {
-    let newColumn = ['','','',''];
-    let column = showCol(j);
-    console.log('Col '+j+': '+column);
-    for (let row = 0; row <= 3;row++) {
-        if (column[row-1] === '' && column[row] !== '' && row-1 < column.length) {
-            newColumn[row-1] = column[row];
-            column[row-1] = column[row];
-            column[row] = '';
+    var colm = showCol(j);
+    for (var k = 0; k <= 3; k++){
+        for (var i = 0; i <= 3; i++){
+            if ((colm[i-1] === '') && (colm[i] !== '')){
+                colm[i-1] = colm[i];
+                colm[i] = '';
+            }
         }
-        else {newColumn[row] = column[row]}
     }
-    console.log('New col '+j+': '+newColumn);
-    setColumn(j,newColumn[0],newColumn[1],newColumn[2],newColumn[3])
+    setColumn(j, colm[0], colm[1], colm[2], colm[3]);
 }
 
 function moveDown(j) {
-    let newColumn = ['','','',''];
-    let column = showCol(j);
-    console.log('Col '+j+': '+column);
-    for (let row = 3; row >= 0;row--) {
-        if (column[row+1] === '' && column[row] !== '' && row+1 < column.length) {
-            newColumn[row+1] = column[row];
-            column[row+1] = column[row];
-            column[row] = '';
+    var colm = showCol(j);
+    for (var k = 0; k <= 3; k++){
+        for (var i = 0; i <= 3; i++){
+            if ((colm[i+1] === '') && (colm[i] !== '')){
+                colm[i+1] = colm[i];
+                colm[i] = '';
+            }
         }
-        else {newColumn[row] = column[row]}
     }
-    console.log('New col '+j+': '+newColumn);
-    setColumn(j,newColumn[0],newColumn[1],newColumn[2],newColumn[3])
+    setColumn(j, colm[0], colm[1], colm[2], colm[3]);
 }
 
 function fusionRight(i) {
@@ -227,6 +211,30 @@ function fusionLeft(i) {
             row[col+1] = 2*Number(getValue(i,col))
             setValue(i,col,'')
             row[col] = ''
+        }
+    }
+}
+
+function fusionUp(j) {
+    let colm = showCol(j);
+    for (let row = 3; row >=0; row--) {
+        if (colm[row] === colm[row-1] && colm[row] !== '') {
+            setValue(row-1,j,2*Number(getValue(row,j)));
+            row[row+1] = 2*Number(getValue(row,j))
+            setValue(row,j,'')
+            row[row] = ''
+        }
+    }
+}
+
+function fusionDown(j) {
+    let colm = showCol(j);
+    for (let row = 0; row <=3; row++) {
+        if (colm[row] === colm[row+1] && colm[row] !== '') {
+            setValue(row+1,j,2*Number(getValue(row,j)));
+            row[row+1] = 2*Number(getValue(row,j))
+            setValue(row,j,'')
+            row[row] = ''
         }
     }
 }
